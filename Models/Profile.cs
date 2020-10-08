80ba44b351ec30b55763cfdf73672884856a1eb9
Nathan Stull <ns811316@ohio.edu>
2020-10-08 16:54:31 -0400

    Running a Test commit

---
+++ Models/Profile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MIS4200_Team8_Scrum.Models
+{
+    public class Profile
+    {
+
+        public int MyProperty { get; set; }
+
+    }
+}
